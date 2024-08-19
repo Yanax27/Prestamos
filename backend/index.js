a7a6622d4f38bc3f5db607c0ef7d4f91a95a511c
yanax27 <luchin.ly50@gmail.com>
2024-08-19 03:46:51 -0400

    feat: added login with cookies in the frontend

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,10 @@ server.use("/", (req, res) => {
 });
 //model to update
 server.use((err, req, res, next) => { //errores cacheados
-  const { statusCode, message } = err;
-  console.log("Muestrame el error ", statusCode, message);
+  /*const { statusCode, message } = err;
+  console.log("Muestrame el error ", statusCode, message);*/
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
   resError(res, statusCode, message);
 });
 
