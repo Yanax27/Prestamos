c56554435820a69538ce1cd49602e305d2f7646d
yanax27 <luchin.ly50@gmail.com>
2024-10-29 01:32:42 -0400

    fix: add and delete prestamo with cuenta

--- backend/src/routes/prestamoRouter.js
+++ backend/src/routes/prestamoRouter.js
@@ -5,6 +5,7 @@ const router = express();
 // Rutas
 router.get('/', prestamoController.getAllPrestamos);//obtener todo
 router.get('/:id', prestamoController.getPrestamoById); // obtener por id
+router.get('/:id', prestamoController.getPrestamoById); // obtener por id
 router.post('/', prestamoController.createPrestamo);//crear prestario
 router.put('/:id', prestamoController.updatePrestamo);// Update a Prestario
 router.delete('/:id', prestamoController.deletePrestamo);// Delete a Prestario
