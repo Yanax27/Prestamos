58181ab2601131dd169a6cfccc2dd11095358de6
yanax27 <luchin.ly50@gmail.com>
2024-10-30 14:31:32 -0400

    fix: modify cards

--- frontend/src/Paginas/ResumenFinanciero.jsx
+++ frontend/src/Paginas/ResumenFinanciero.jsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
-import { getDoc, doc } from "firebase/firestore";
-import { db } from "../data/FIreBase";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css";
-import { FiDollarSign, FiTrendingUp, FiTrendingDown } from "react-icons/fi";
-import "../styles/ResumenFinanciero.css";
 import { Spinner } from "../components/Spinner";
 import { DataContext } from "../context/Provider";
 import config from "../config";
+import { fetchGetAllUsuarios } from "../http/fetchUsuario";
+import { fetchGetCuentaById } from "../http/fetchCuenta";
+import CardDataCuenta from "../components/CardDataCuenta"; // Asegúrate de que este componente esté adaptado para usar las props correctamente.
 
 const BancoImagenes = [
   "https://i.ibb.co/MRyknnn/Capital.png",
@@ -16,56 +13,44 @@ const BancoImagenes = [
   "https://i.ibb.co/gWFxgJx/Egresos.png",
   "https://i.ibb.co/KmTJx5H/Ingreso.png",
 ];
+
 const ResumenFinanciero = () => {
   const [cuentaData, setCuentaData] = useState({});
   const [arrayData, setArrayData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { authUser, setDataAuth } = useContext(DataContext);
+  const [email, setEmail] = useState(localStorage.getItem('userEmail') || '');
 
   useEffect(() => {
     const fetchCuentaData = async () => {
       try {
-        const cuentaDocRef = doc(db, "CuentaDB", "yIIKrEjqotEpdqwAK2Bv");
-        const cuentaDocSnap = await getDoc(cuentaDocRef);
-
-        if (cuentaDocSnap.exists()) {
-          const data = cuentaDocSnap.data();
-          // Inicializa el array aquí
-          const newArrayData = [];
-          newArrayData.push({
-            Capital: data.Capital.toFixed(2),
-            descripcion: "Capital",
-          });
-          newArrayData.push({
-            "Caja actual": data.Caja_Actual.toFixed(2),
-            descripcion: "Caja",
-          });
-          newArrayData.push({
-            "Total prestamos": data.Total_Prestamos.toFixed(2),
-            descripcion: "Prestamo",
-          });
-          newArrayData.push({
-            Egresos: data.Egresos.toFixed(2),
-            descripcion: "Egresos",
-          });
-          newArrayData.push({
-            Ingresos: data.Ingresos.toFixed(2),
-            descripcion: "Ingreso",
-          });
-          setArrayData(newArrayData);
-          setCuentaData(data);
+        setLoading(true);
+        const usuario = await fetchGetAllUsuarios(email);
+        if (usuario && usuario[0]) {
+          const cuenta = await fetchGetCuentaById(usuario[0].CuentumIdCuenta);
+          if (cuenta) {
+            const newArrayData = [
+              { title: "Capital", total: cuenta.capital.toFixed(2), rate: "0.5%", levelUp: true },
+              { title: "Caja Actual", total: cuenta.cajaActual.toFixed(2), rate: "0.8%", levelUp: true },
+              { title: "Ventas", total: cuenta.ventas.toFixed(2), rate: "1.2%", levelUp: true },
+              { title: "Caja Ultima Liquidada", total: cuenta.cajaUltimaLiquidacion.toFixed(2), rate: "1.2%", levelUp: true },
+              // Agrega otros datos que necesites de `cuenta`
+            ];
+            setArrayData(newArrayData);
+            setCuentaData(cuenta);
+          } else {
+            console.log("No se encontró la cuenta del usuario");
+          }
         } else {
-          console.log("No such document!");
+          console.log("Usuario no encontrado");
         }
-        setLoading(false);
       } catch (error) {
-        console.error("Error fetching document: ", error);
+        console.error("Error al obtener los datos de la cuenta: ", error);
+      } finally {
         setLoading(false);
       }
     };
-
     fetchCuentaData();
-  }, []);
+  }, [email]);
 
   const renderResumenFinanciero = () => {
     if (loading) {
@@ -76,47 +61,23 @@ const ResumenFinanciero = () => {
       );
     }
 
-    const cajaActualPercentage =
-      (cuentaData.Caja_Actual / cuentaData.Capital) * 100;
-    const interesPercentage = (cuentaData.Intereses / cuentaData.Capital) * 100;
     return (
-      <div className="grid grid-cols-6 w-full ">
-        <div className="flex flex-cols col-span-6 p-2 gap-4">
-          {arrayData.map((obj, index) => {
-            const key = Object.keys(obj)[0];
-            const value = obj[key];
-            return (
-              <div
-                key={key}
-                className={`
-                  w-1/5 
-                  ${`${config.colors[index]}`} 
-                  rounded-lg overflow-hidden 
-                  shadow-lg border p-4 flex flex-col hover:cursor-pointer
-                  transform hover:scale-105 hover:shadow-lg transition-all duration-200`}
-              >
-                <div className="flex items-end justify-end mb-4">
-                  <img src={BancoImagenes[index]} className="w-12" alt="" />
-                </div>
-                <div className="flex items-center justify-between mb-4">
-                  <span className="text-white text-sm">{key}</span>
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="text-right">
-                    <span className="text-white text-xl font-bold text-white">
-                      {value} <span className="text-white">$</span>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-        </div>
-        <div className="col-span-2 "></div>
-        <div className="col-span-4 "></div>
+      <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        {arrayData.map((item, index) => (
+          <CardDataCuenta
+            key={index}
+            title={item.title}
+            total={`Bs.${item.total}`}
+            rate={item.rate}
+            levelUp={item.levelUp}
+          >
+            <img src={BancoImagenes[index]} className="w-12" alt={`${item.title} icon`} />
+          </CardDataCuenta>
+        ))}
       </div>
+      </>
+      
     );
   };
 
