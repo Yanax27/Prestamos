294fcf1e5f87b927d7d8bcd716417464c73ee9e6
yanax27 <luchin.ly50@gmail.com>
2024-11-23 17:33:31 -0400

    style: table clients

--- frontend/src/components/Clientes.jsx
+++ frontend/src/components/Clientes.jsx
@@ -3,7 +3,10 @@ import { FaPlus, FaEdit, FaTrash, FaSearch } from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
 import { Spinner } from "../components/Spinner";
 import { useLocation } from "react-router-dom";
-import { fetchGetAllPrestarios, fetchDeletePrestario } from "../http/fetchPrestario"; // Cambiar la ruta según tu estructura
+import swal from "sweetalert";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { fetchGetAllPrestarios, fetchDeletePrestario } from "../http/fetchPrestario";
 
 const Clientes = () => {
   const [datos, setDatos] = useState([]);
@@ -14,9 +17,8 @@ const Clientes = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetchGetAllPrestarios(); 
+        const data = await fetchGetAllPrestarios();
         setDatos(data);
-        //console.log(datos)
         setLoading(false);
       } catch (error) {
         console.error("Error fetching prestarios:", error);
@@ -24,19 +26,27 @@ const Clientes = () => {
     };
     fetchData();
   }, []);
-  
+
   const handleDelete = async (id, nombre) => {
-    if (window.confirm(`¿Estás seguro de que deseas eliminar al Cliente ${nombre}?`)) {
-      try {
-        await fetchDeletePrestario(id);
-        alert(`Se ha eliminado al Cliente ${nombre}`);
-        // Vuelve a obtener la lista actualizada de prestarios
-        const updatedDatos = await fetchGetAllPrestarios();
-        setDatos(updatedDatos);
-      } catch (error) {
-        console.error("Error deleting prestario:", error);
+    swal({
+      title: "¿Estás seguro?",
+      text: `¿Quieres eliminar al cliente ${nombre}?`,
+      icon: "warning",
+      buttons: ["Cancelar", "Eliminar"],
+      dangerMode: true,
+    }).then(async (willDelete) => {
+      if (willDelete) {
+        try {
+          await fetchDeletePrestario(id);
+          toast.success(`Cliente ${nombre} eliminado exitosamente`);
+          const updatedDatos = await fetchGetAllPrestarios();
+          setDatos(updatedDatos);
+        } catch (error) {
+          toast.error("Error al eliminar el cliente");
+          console.error("Error deleting prestario:", error);
+        }
       }
-    }
+    });
   };
 
   const handleSearch = (event) => {
@@ -49,92 +59,134 @@ const Clientes = () => {
   });
 
   return (
-    <main className=".">
-      <section className="p-3">
-        <p className="text-gray-500 dark:text-gray-400">
-          {location.pathname}
-        </p>  
-        <div className="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
-          <div className="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
-            <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+    <div className="grid w-full">
+      <ToastContainer />
+      <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+        <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+          <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+              {/* Búsqueda */}
               <div className="w-full md:w-1/2">
                 <form className="flex items-center">
-                  <label htmlFor="simple-search" className="sr-only">Buscar</label>
+                  <label htmlFor="simple-search" className="sr-only">
+                    Buscar
+                  </label>
                   <div className="relative w-full">
                     <input
                       type="text"
                       id="simple-search"
                       className="block w-full p-2 pl-3 pr-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       placeholder="Buscar..."
-                      required=""
                       value={searchTerm}
                       onChange={handleSearch}
                     />
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-3">
                       <FaSearch className="w-5 h-5 text-gray-500 dark:text-gray-400" />
                     </div>
                   </div>
                 </form>
               </div>
-              <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+
+              {/* Botones Agregar y Filtrar */}
+              <div className="flex items-center space-x-3 w-full md:w-auto">
                 <Link to={"/dashboard/agregar/cliente/newClient"}>
-                  <button type="button" className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
+                  <button
+                    type="button"
+                    className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary hover:bg-secondary focus:ring-4 focus:ring-secondary dark:bg-primary dark:hover:bg-primary focus:outline-none dark:focus:ring-secondary"
+                  >
                     <FaPlus className="h-3.5 w-3.5 mr-2 -ml-1" />
                     Añadir Cliente
                   </button>
                 </Link>
+
+                {/* Botón de Filtro */}
+                <button
+                  id="filterDropdownButton"
+                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  type="button"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="h-4 w-4 mr-2 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  Filtrar
+                  <svg
+                    className="-mr-1 ml-1.5 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    />
+                  </svg>
+                </button>
               </div>
+            </div>
+            
+            {/* Tabla */}
+            <div className="overflow-x-auto">
+              {loading ? (
+                <div className="flex justify-center items-center">
+                  <Spinner />
+                </div>
+              ) : (
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">Nombres</th>
+                      <th scope="col" className="px-6 py-3">Apellidos</th>
+                      <th scope="col" className="px-6 py-3">Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {filteredDatos.map((cliente) => (
+                      <tr key={cliente.id_prestario} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td className="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                          <NavLink to={`/dashboard/detalle/cliente/${cliente.id_prestario}`}>
+                            {cliente.nombre}
+                          </NavLink>
+                        </td>
+                        <td className="px-6 py-4">
+                          <NavLink to={`/dashboard/detalle/cliente/${cliente.id_prestario}`}>
+                            {cliente.apellido}
+                          </NavLink>
+                        </td>
+                        <td className="px-6 py-4 flex space-x-2">
+                          <Link to={`/dashboard/agregar/cliente/${cliente.id_prestario}`}>
+                            <button className="text-yellow-600 hover:text-yellow-800">
+                              <FaEdit />
+                            </button>
+                          </Link>
+                          <button
+                            onClick={() => handleDelete(cliente.id_prestario, cliente.nombre)}
+                            className="text-red-600 hover:text-red-800"
+                          >
+                            <FaTrash />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </div>
           </div>
         </div>
       </section>
-
-      {loading ? (
-        <div className="flex-1 flex justify-center items-center">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3">Nombres</th>
-                <th scope="col" className="px-6 py-3">Apellidos</th>
-                <th scope="col" className="px-6 py-3">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredDatos.map((cliente) => (
-                <tr key={cliente.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                  <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    <NavLink to={`/dashboard/detalle/cliente/${cliente.id_prestario}`} className="navlink-item">
-                      {cliente.nombre}
-                    </NavLink>
-                  </th>
-                  <td className="px-6 py-4">
-                    <NavLink to={`/dashboard/detalle/cliente/${cliente.id_prestario}`} className="navlink-item">
-                      {cliente.apellido}
-                    </NavLink>
-                  </td>
-                  <td className="px-6 py-4">
-                    <span className="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
-                      <button className="text-yellow-600 inline-block border-e p-3 text-gray-700 hover:bg-gray-50 focus:relative" title="Edit Client">
-                        <Link to={`/dashboard/agregar/cliente/${cliente.id_prestario}`} className="navlink-button">
-                          <FaEdit className="h-4 w-4" />
-                        </Link>
-                      </button>
-                      <button className="text-red-700 inline-block p-3 text-gray-700 hover:bg-gray-50 focus:relative" title="Delete Client" onClick={() => handleDelete(cliente.id_prestario, cliente.nombre)}>
-                        <FaTrash className="h-4 w-4" />
-                      </button>
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-    </main>
+    </div>
   );
 };
 
