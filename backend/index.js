0569d60a8f7b537929f50e3d12b84a5066ec33f8
yanax27 <luchin.ly50@gmail.com>
2024-11-26 02:19:08 -0400

    fix: modified production

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
-require("dotenv").config(); //aqui .env
+require("dotenv").config();
 const server = require("./src/app");
 const { conn } = require("./src/db");
 const resError = require("./src/utils/resError");
-const { PORT } = process.env;
-const path = require("path");
+
+const PORT = process.env.PORT || 3001;
 
 server.use("/", (req, res) => {
+  // Puedes servir archivos estáticos aquí si es necesario
   // res.status(200).sendFile(path.join(__dirname, "./src/html/index.html"));
 });
-//model to update
-server.use((err, req, res, next) => { //errores cacheados
-  /*const { statusCode, message } = err;
-  console.log("Muestrame el error ", statusCode, message);*/
+
+server.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   resError(res, statusCode, message);
 });
 
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log("SERVER IS RUNNING IN PORT ",PORT);
+    console.log("SERVER IS RUNNING ON PORT", PORT);
   });
 });
