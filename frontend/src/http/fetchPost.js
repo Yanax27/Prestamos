f5b841797a701844bff2ba9577fc7b68fe61e897
yanax27 <luchin.ly50@gmail.com>
2024-11-26 01:37:04 -0400

    fix: deailclient and cuenta

--- frontend/src/http/fetchPost.js
+++ frontend/src/http/fetchPost.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import config from "../config";
 
+// Iniciar sesión
 export const loginUser = async (body) => {
-    //return await axios.post(`${config.typeRequest}://${config.apiUrl}/usuario/login`,body);
     try {
         return await axios.post(`${config.typeRequest}://${config.apiUrl}/usuario/login`, body);
     } catch (error) {
@@ -12,6 +12,17 @@ export const loginUser = async (body) => {
             return { error: true, message: error.message }; // Devuelve un error general
         }
     }
-
 };
 
+// Cerrar sesión
+export const logoutUser = async () => {
+    try {
+        return await axios.post(`${config.typeRequest}://${config.apiUrl}/usuario/logout`);
+    } catch (error) {
+        if (error.response) {
+            return error.response.data; // Devuelve los datos del error del backend
+        } else {
+            return { error: true, message: error.message }; // Devuelve un error general
+        }
+    }
+};
